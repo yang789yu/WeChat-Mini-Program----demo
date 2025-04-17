6e57bf3d69e168f33143c42f9a1dfd152537f76d
“zhangx” <2695926793@qq.com>
2025-04-17 22:34:34 +0800

    feat: 搭建app.json

--- app.js
+++ app.js
@@ -1,17 +1,16 @@
 // app.js
 App({
-  onLaunch() {
-    // 展示本地存储能力
-    const logs = wx.getStorageSync('logs') || []
-    logs.unshift(Date.now())
-    wx.setStorageSync('logs', logs)
+  onLaunch(options) {
 
-    // 登录
-    wx.login({
-      success: res => {
-        // 发送 res.code 到后台换取 openId, sessionKey, unionId
-      }
-    })
+  },
+  onShow(options) {
+
+  },
+  onHide(options) {
+
+  },
+  onError(options) {
+
   },
   globalData: {
     userInfo: null
